0c6aa40c4565bf000ebcb3920fb7be4897336cdb
Kamryn Smith <programersign@gmail.com>
2023-04-07 10:17:14 -0700

    assignment complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  function pDisplay() {
+    if (!props.bio) {
+      return null
+    } else {
+      return <p>{props.bio}</p>
+    }
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {pDisplay()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
     </div>
   );
 }
+
+
+
 
 export default About;
